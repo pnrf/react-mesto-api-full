fe5c1343f1c94a2c46821d611ad1b8cded62d88f
Pankratov <pnrf@yandex.ru>
2022-09-13 11:25:20 +0300

    fix: bugfix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,15 +4,20 @@ const auth = require('../middlewares/auth');
 const userRouters = require('./users');
 const cardRouters = require('./cards');
 const { validateSignUp, validateSignIn } = require('../middlewares/validators');
-const { createUser, login, logout } = require('../controllers/users');
+const {
+  createUser, login, logout, checkCookies,
+} = require('../controllers/users');
 
 const { NotFoundError } = require('../errors/index-errors');
+// const { checkCookies } = require('../../frontend/src/utils/apiAuth');
 
 const router = express.Router();
 
 router.post('/signup', validateSignUp, createUser);
 router.post('/signin', validateSignIn, login);
 router.get('/signout', logout);
+
+router.get('/check', checkCookies);
 
 router.use('/users', auth, userRouters);
 router.use('/cards', auth, cardRouters);
